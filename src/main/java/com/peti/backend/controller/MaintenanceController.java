678673f9f31445df485a9b0fae29574e1d4c23a9
tiforn <tarasfqx@gmail.com>
2025-06-10 20:38:03 +0300

    Add swagger for backend

--- src/main/java/com/peti/backend/controller/MaintenanceController.java
+++ src/main/java/com/peti/backend/controller/MaintenanceController.java
@@ -1,5 +1,6 @@
 package com.peti.backend.controller;
 
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -7,6 +8,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping(value = "/api")
+@Tag(name = "Maintenance", description = "Endpoint only needed for maintenance don't expose them publicly")
 public class MaintenanceController {
 
   @GetMapping("/ping")
