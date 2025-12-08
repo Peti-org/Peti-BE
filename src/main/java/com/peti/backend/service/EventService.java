d367e5c1d49b0419be51cf32709c7d6012272931
Taras Leshchyshyn <52927358+lyndexter@users.noreply.github.com>
2025-12-08 20:20:31 +0200

    Add event functionality & fix bugs (#12)

--- src/main/java/com/peti/backend/service/EventService.java
+++ src/main/java/com/peti/backend/service/EventService.java
@@ -1,46 +1,87 @@
 package com.peti.backend.service;
 
-import com.peti.backend.model.domain.Event;
-import com.peti.backend.repository.EventRepository;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
+import static com.peti.backend.service.CaretakerService.convertToSimpleDto;
 
+import com.peti.backend.dto.EventDto;
+import com.peti.backend.dto.PriceDto;
+import com.peti.backend.dto.slot.SlotDto;
+import com.peti.backend.model.domain.Caretaker;
+import com.peti.backend.model.domain.Event;
+import com.peti.backend.model.domain.Slot;
+import com.peti.backend.model.domain.User;
+import com.peti.backend.repository.EventRepository;
+import jakarta.persistence.EntityManager;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
 import java.util.List;
-import java.util.Optional;
+import java.util.UUID;
+import java.util.stream.Collectors;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
+@RequiredArgsConstructor
 public class EventService {
 
-    @Autowired
-    private EventRepository eventRepository;
+  private final EventRepository eventRepository;
+  private final EntityManager entityManager;
 
-    public List<Event> getAllEvents() {
-        return eventRepository.findAll();
+
+  public static EventDto convertToDto(Event event) {
+    return new EventDto(
+        event.getEventId(),
+        convertToSimpleDto(event.getCaretaker()),
+        UserService.convertToDto(event.getUser(), CityService.convertToDto(event.getUser().getCityByCityId())),
+        SlotService.convertToDto(event.getSlot()),
+        event.getPrice(),
+        event.getStatus()
+    );
+  }
+
+  @Transactional
+  public EventDto createEvent(SlotDto slotDto, UUID userId) {
+    // create event
+    Event event = toEvent(slotDto, userId);
+    Event saved = eventRepository.save(event);
+    return convertToDto(saved);
+  }
+
+  public List<EventDto> getEventsByCaretakerId(UUID caretakerId) {
+    List<Event> events = eventRepository.findAllByCaretaker_CaretakerId(caretakerId);
+
+    return events.stream()
+        .map(EventService::convertToDto)
+        .collect(Collectors.toList());
+  }
+
+  public List<EventDto> getEventsByUserId(UUID userId) {
+    List<Event> events = eventRepository.findAllByUser_UserId(userId);
+
+    return events.stream()
+        .map(EventService::convertToDto)
+        .collect(Collectors.toList());
+  }
+
+  public boolean deleteEvent(UUID eventId, UUID userId) {
+    if (eventRepository.existsByEventIdAndUser_UserId(eventId, userId)) {
+      eventRepository.deleteById(eventId);
+      return true;
     }
+    return false;
+  }
 
-    public Optional<Event> getEventById(Long id) {
-        return eventRepository.findById(id);
-    }
+  private Event toEvent(SlotDto slotDto, UUID userId) {
+    Event event = new Event();
 
-    public Event createEvent(Event event) {
-        return eventRepository.save(event);
-    }
+    event.setCaretaker(entityManager.getReference(Caretaker.class, slotDto.caretaker().getId()));
+    event.setUser(entityManager.getReference(User.class, userId));
+    event.setSlot(entityManager.getReference(Slot.class, slotDto.slotId()));
+    event.setPrice(new PriceDto(slotDto.price(), slotDto.currency(), new ArrayList<>()));
+    event.setStatus("created");
+    event.setCreatedAt(LocalDateTime.now());
+    event.setEventIsDeleted(false);
 
-//    public Optional<Event> updateEvent(Long id, Event eventDetails) {
-//        return eventRepository.findById(id).map(event -> {
-//            event.setName(eventDetails.getName());
-//            event.setDescription(eventDetails.getDescription());
-//            event.setDate(eventDetails.getDate());
-//            event.setLocation(eventDetails.getLocation());
-//            return eventRepository.save(event);
-//        });
-//    }
-
-    public boolean deleteEvent(Long id) {
-        if (eventRepository.existsById(id)) {
-            eventRepository.deleteById(id);
-            return true;
-        }
-        return false;
-    }
+    return event;
+  }
 }
